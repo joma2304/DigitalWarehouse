e8495bdeddfab8c3ec3d647d51a86b246715295d
Johan Magnusson <joma2304@student.miun.se>
2025-03-11 15:29:41 +0100

    Lagt till skyddad för delar med admin, stylat om delar och titlar

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DigitalWarehouse.Data;
 using DigitalWarehouse.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DigitalWarehouse.Controllers
 {
+    [Authorize (Roles = "Admin")]
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _context;
