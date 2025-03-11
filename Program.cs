6a04bec328b5e31b8f4075eab79db261f74dd1ff
Johan Magnusson <joma2304@student.miun.se>
2025-03-11 14:54:54 +0100

    Lagt till en admin användare

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
@@ -42,5 +43,32 @@ app.MapControllerRoute(
 
 app.MapRazorPages()
    .WithStaticAssets();
+
+//Skapa Adminroll och användare till dessa
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    //Adminroll
+    var role = "Admin";
+    if (!await roleManager.RoleExistsAsync(role))
+    {
+        await roleManager.CreateAsync(new IdentityRole(role));
+    }
+
+    //Skapa administratör
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var user = new { Email = "admin@warehouse.com", UserName = "admin@warehouse.com", Password = "Admin!123", Role = "Admin" };
+    ;
+    var IdentityUser = await userManager.FindByEmailAsync(user.Email);
+    if (IdentityUser == null)
+    {
+        IdentityUser = new IdentityUser { UserName = user.UserName, Email = user.Email };
+        await userManager.CreateAsync(IdentityUser, user.Password);
+
+        //Lägg till admin till Användaren
+        await userManager.AddToRoleAsync(IdentityUser, user.Role);
+    }
+}
 
 app.Run();
